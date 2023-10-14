089e105d131ebfe63e9650532260bbfecaeed0b8
ccskumasan <86587248+ccskumasan@users.noreply.github.com>
2023-10-14 14:56:35 -0700

    Update script.js -- add button functionality

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ document.body.appendChild(newButton);*/
 
 function onButtonClick(){
   var buttonClick = 1;
+  alert("Button clicked);
 }
+
+const button = document.querySelector('button);
+button.addEventListener('click', onButtonClick());
+
 
