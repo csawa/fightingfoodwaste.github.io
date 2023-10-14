b95680ce37754bd99bef1001df1c2ad2e9c10585
ccskumasan <86587248+ccskumasan@users.noreply.github.com>
2023-10-14 14:51:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const newButton = document.createElement('button');
+/*const newButton = document.createElement('button');
 newButton.textContent = 'Click me!';
-document.body.appendChild(newButton);
+document.body.appendChild(newButton);*/
+
+function onButtonClick(){
+  var buttonClick = 1;
+}
 
